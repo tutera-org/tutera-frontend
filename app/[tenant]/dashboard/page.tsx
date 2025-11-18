39ebe493bb6021998a078628ed4e5c5be00ad4ab
Okpara202 <okparafavour202@gmail.com>
2025-11-18 15:03:38 +0100

    Quiz page done

--- app/[tenant]/dashboard/page.tsx
+++ app/[tenant]/dashboard/page.tsx
@@ -3,7 +3,7 @@ import CreatorPage from "@/components/creatorDashboard/CreatorPage";
 import StudentPage from "@/components/creatorDashboard/StudentPage";
 
 export default function DashboardPage() {
-  const role = "creator" as "creator" | "student";
+  const role = "student" as "creator" | "student";
   return (
     <div className="mt-10 md:mt-14 w-full">
       {role === "creator" ? <CreatorPage /> : <StudentPage />}
